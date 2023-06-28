a575d588dd37c9689162c7029152a5b305c36b7b
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-28 14:45:04 -0300

    commit 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,37 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+// import 'dart:math';
+import 'package:flutter_testes_de_integracao/components/hamburger_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_testes_de_integracao/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('Testing the Menu', () {
+    testWidgets('Hamburguer Menu should have "Menu"', (tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: HamburgerMenu(),
+      ));
+      expect(find.text('Menu'), findsOneWidget);
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    testWidgets('Hamburguer Menu should have "Gerenciar clientes"',
+        (tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: HamburgerMenu(),
+      ));
+      expect(find.text('Gerenciar clientes'), findsOneWidget);
+    });
+    testWidgets('Hamburguer Menu should have "Tipos de clientes"',
+        (tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: HamburgerMenu(),
+      ));
+      expect(find.text('Tipos de clientes'), findsOneWidget);
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    testWidgets('Hamburguer Menu should have "Sair"', (tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: HamburgerMenu(),
+      ));
+      expect(find.text('Sair'), findsOneWidget);
+    });
   });
 }
